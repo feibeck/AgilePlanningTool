01c1d2a771f64699b5f6707e9c18fbc2782c0ff5
Florian Eibeck <florian.eibeck@mayflower.de>
2010-05-12 17:44:45 +0200

    Added layout and view initialisation
    Added configuration for Zend_Layout, added a layout view-script and initialized the view

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,24 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+    /**
+     * Initialise the view
+     *
+     * @return Zend_View The view instance
+     */
+    protected function _initView()
+    {
+        $view = new Zend_View();
+        $view->doctype('XHTML1_STRICT');
+        $view->headTitle('AgilePlanningTool');
+
+        $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper(
+            'ViewRenderer'
+        );
+
+        $viewRenderer->setView($view);
+
+        return $view;
+    }
 
 }
-
