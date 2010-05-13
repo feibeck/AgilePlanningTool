d56c366d706f01b4eda8d820c389bbcd1ac64556
Tobias Schlueter <github@tobbls.de>
2010-05-13 11:02:36 +0200

    Basic Acl implementation.

---
+++ library/Apt/Acl.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Acl
+ *
+ * @category  AgilePlanningTool
+ * @package   Apt_Acl
+ * @author    Tobias Schlüter <tobias.schlueter@mayflower.de>
+ * @copyright 2010 Mayflower GmbH
+ * @license   New BSD License
+ * @version   $Id$
+ */
+
+/**
+ * Acl 
+ * 
+ * @category  AgilePlanningTool
+ * @package   Apt_Acl
+ * @author    Tobias Schlüter <tobias.schlueter@mayflower.de>
+ * @copyright 2010 Mayflower GmbH
+ * @license   New BSD License
+ * @version   $Id$
+ */
+ class Apt_Acl extends Zend_Acl
+ {
+     /**
+      * Setup Acl.
+      */
+     public function __construct()
+     {
+         $this->_initRoles();
+         $this->_initResources();
+         $this->_initPermissions();
+     }
+     
+     /**
+      * Initialize roles.
+      */
+     protected _initRoles()
+     {
+         $this->addRole(new Zend_Acl_Role('guest'));
+         $this->addRole(new Zend_Acl_Role('user'), 'guest');
+         $this->addRole(new Zend_Acl_Role('productowner'), 'user');
+     }
+     
+     /**
+      * Initialize resources.
+      */
+     protected _initResources()
+     {
+         $this->add(new Zend_Acl_Resource('project'));
+         $this->add(new Zend_Acl_Resource('backlog'));
+     }
+     
+     /**
+      * Initialize permissions.
+      */
+     protected _initPermissions()
+     {
+         // guest
+         $this->deny('guest', 'project', 'view');
+         $this->deny('guest', 'project', 'edit');
+         $this->deny('guest', 'backlog', 'view');
+         $this->deny('guest', 'backlog', 'edit');
+         
+         // user
+         $this->allow('user', 'project', 'view');
+         $this->allow('user', 'backlog', 'view');
+         
+         // productowner
+         $this->allow('productowner', 'project', 'edit');
+         $this->allow('productowner', 'backlog', 'edit');
+     }
+ }
