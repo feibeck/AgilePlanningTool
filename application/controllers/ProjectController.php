a3436382fa88587107d9fbd271caf25071b2ec61
Florian Eibeck <florian.eibeck@mayflower.de>
2010-05-13 18:36:16 +0200

    Adding projects functionality
    Added form and controller action for creating new projects

--- application/controllers/ProjectController.php
+++ application/controllers/ProjectController.php
@@ -24,14 +24,49 @@ class ProjectController extends Zend_Controller_Action
 {
 
     /**
+     * Doctrine Entity Manager instance
+     *
+     * @var \Doctrine\ORM\EntityManager
+     */
+    protected $_em;
+
+    public function init()
+    {
+        $bootstrap = $this->getInvokeArg('bootstrap');
+        $this->_em = $bootstrap->getResource('doctrine');
+    }
+
+    /**
      * Shows the users project list.
      */
     public function indexAction()
     {
-        $bootstrap = $this->getInvokeArg('bootstrap');
-        $em = $bootstrap->getResource('doctrine');
-        $this->view->projectList = $em->getRepository('Apt_Model_Project')->findAll();
+        $this->view->projectList = $this->_em->getRepository('Apt_Model_Project')->findAll();
     }
 
+    /**
+     * Add a new Project
+     */
+    public function addAction()
+    {
+        $form = new Apt_Form_Project(array('method' => 'POST'));
+
+        $request = $this->getRequest();
+        if ($request->isPost() && $form->isValid($request->getPost())) {
+
+            $project = new Apt_Model_Project();
+            $project->name = $form->getValue('name');
+            $project->sprintLength = $form->getValue('length');
+            $project->defaultVelocity  = $form->getValue('velocity');
+
+            $this->_em->persist($project);
+            $this->_em->flush();
+
+            $this->_helper->redirector->gotoSimple('index');
+
+        }
+
+        $this->view->form = $form;
+    }
 }
 
